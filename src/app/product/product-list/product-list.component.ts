a32fbe152bcb14b3a3d188bf0adb740c7189ad04
Miguel Rivera <riveragarcia.miguelangel.00e3@gmail.com>
2024-09-19 21:04:39 -0600

    Segundo commit jueves 19

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  mostrarImagen():void{
+  this.muestraImg=!this.muestraImg
+  }
+
+  productos:any[]=[
+    {
+      "ProductoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"2 Puertas",
+      "Year":"febrero 2 2020",
+      "Precio":20000,
+      "Color":"Blanco",
+      "Marca":"Nissan",
+      "ImagenUrl":"https://autoprice.com.mx/imgs/fotos/3622/98b4fced9c8c47a7_ap.jpg"
+    },
+    {
+      "ProductoId":2,
+      "Modelo":"A4",
+      "Descripcion":"4 Puertas",
+      "Year":"marzo 12 2021",
+      "Precio":120000,
+      "Color":"Blanco",
+      "Marca":"AUDI",
+      "ImagenUrl":"https://file.kelleybluebookimages.com/kbb/base/evox/CP/15034/2021-Audi-A4-front_15034_032_1970x741_T9T9_cropped.png"
+    },
+    {
+      "ProductoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"2 Puertas",
+      "Year":"febrero 2 2020",
+      "Precio":18000,
+      "Color":"Azul",
+      "Marca":"KIA",
+      "ImagenUrl":"https://http2.mlstatic.com/D_NQ_NP_624789-MLM77736833604_072024-O.webp"
+    }
+  ]
 }
